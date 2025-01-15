3f79a7f2c2576efe7c9e3bdc6168efc7edfd2bfb
yyasuda <y.yasuda@sinary.com>
2025-01-15 12:09:14 +0900

    up

---
+++ SampleFormApp_Test/Test1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleFormsApp;
+using System.Windows.Forms;
+
+namespace SampleFormApp_Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            int expected = 7;
+            var main = new MainForm();
+            var actual = main.Sum(5, 2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
